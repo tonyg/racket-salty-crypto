01ac01e114059143abdff873cd162afe1a888f17
Tony Garnock-Jones <tonyg@leastfixedpoint.com>
2023-01-18 16:06:26 +0100

    psk tests

--- noise/main.rkt
+++ noise/main.rkt
@@ -7,18 +7,20 @@
          DecryptWithAd
          Rekey
 
-         make-initiator
-         make-responder
+         make-HandshakeState
          WriteMessage
          ReadMessage
 
-         ;; plus uses of define/provide below
+         (struct-out handshake-pattern)
+         lookup-handshake-pattern
          )
 
 (require libsodium)
 (require libb2)
 (require racket/match)
 (require racket/port)
+(require (only-in racket/string string-split))
+(require (only-in racket/list take drop))
 
 (module+ test (require rackunit))
 
@@ -167,7 +169,14 @@
 
 (struct HandshakeState (ss s e rs re role message-patterns psks) #:mutable)
 
-(define (make-HandshakeState handshake_pattern role prologue s e rs re [psks #f])
+(define (make-HandshakeState handshake_pattern
+                             #:role role
+                             #:prologue [prologue #""]
+                             #:static-keypair [s #f]
+                             #:remote-static-pk [rs #f]
+                             #:pregenerated-ephemeral-keypair [e #f]
+                             #:remote-pregenerated-ephemeral-pk [re #f]
+                             #:preshared-keys [psks #f])
   (define protocol_name
     (string->bytes/utf-8 (format "Noise_~a_25519_ChaChaPoly_BLAKE2s"
                                  (handshake-pattern-name handshake_pattern))))
@@ -184,22 +193,6 @@
                ['e (match role ['initiator re] ['responder (crypto-box-keypair-pk e)])]
                ['s (match role ['initiator rs] ['responder (crypto-box-keypair-pk s)])])))
   (HandshakeState ss s e rs re role (handshake-pattern-message-patterns handshake_pattern) psks))
-
-(define (make-initiator handshake_pattern
-                        #:prologue [prologue #""]
-                        #:static-keypair [static #f]
-                        #:remote-static-pk [remote-static #f]
-                        #:pregenerated-ephemeral-keypair [ephemeral #f]
-                        #:preshared-keys [psks #f])
-  (make-HandshakeState handshake_pattern 'initiator prologue static ephemeral remote-static #f psks))
-
-(define (make-responder handshake_pattern
-                        #:prologue [prologue #""]
-                        #:static-keypair [static #f]
-                        #:remote-static-pk [remote-static #f]
-                        #:pregenerated-ephemeral-keypair [ephemeral #f]
-                        #:preshared-keys [psks #f])
-  (make-HandshakeState handshake_pattern 'responder prologue static ephemeral remote-static #f psks))
 
 (define (next-message-pattern! hs)
   (match-define (cons mp more) (HandshakeState-message-patterns hs))
@@ -273,16 +266,45 @@
 
 ;;---------------------------------------------------------------------------
 
-(define-syntax-rule (define/provide id expr) (begin (define id expr) (provide id)))
+(define (lookup-handshake-pattern pattern-name-str)
+  (match pattern-name-str
+    [(pregexp #px"^([NKX]|[NKXI]1?[NKX]1?)([a-z][a-z0-9]*(\\+[a-z][a-z0-9]*)*)?$"
+              (list _ main modifiers0 _final-modifier))
+     (define modifiers (string-split (or modifiers0 "") "+"))
+     (define base
+       (match main
+         ["NN" (handshake-pattern "NN" '() '() '((e) (e ee)))]
+         ["NK" (handshake-pattern "NK" '() '(s) '((e es) (e ee)))]
+         ["IK" (handshake-pattern "IK" '() '(s) '((e es s ss) (e ee se)))]
+         ["XK" (handshake-pattern "XK" '() '(s) '((e es) (e ee) (s se)))]
+         ["XX" (handshake-pattern "XX" '() '() '((e) (e ee s es) (s se)))]
+         ["IX" (handshake-pattern "IX" '() '() '((e s) (e ee se s es)))]
+         [_ #f]))
+     (define modified (for/fold [(p base)] [(mod modifiers)] (and p (apply-modifier p mod))))
+     (and modified (struct-copy handshake-pattern modified [name pattern-name-str]))]
+    [_ #f]))
 
-(define/provide NN (handshake-pattern "NN" '() '() '((e) (e ee))))
-(define/provide NK (handshake-pattern "NK" '() '(s) '((e es) (e ee))))
-(define/provide IK (handshake-pattern "IK" '() '(s) '((e es s ss) (e ee se))))
-(define/provide XK (handshake-pattern "XK" '() '(s) '((e es) (e ee) (s se))))
-(define/provide XX (handshake-pattern "XX" '() '() '((e) (e ee s es) (s se))))
-(define/provide IX (handshake-pattern "IX" '() '() '((e s) (e ee se s es))))
+(define (apply-modifier p mod)
+  (match mod
+    [(pregexp #px"^psk([0-9]+)$" (list _ nstr))
+     (define old-message-patterns (handshake-pattern-message-patterns p))
+     (struct-copy handshake-pattern p
+       [message-patterns
+        (match (string->number nstr)
+          [0 (cons (cons 'psk (car old-message-patterns)) (cdr old-message-patterns))]
+          [n-plus-one (let ((n (- n-plus-one 1)))
+                        (append (take old-message-patterns n)
+                                (let ((ps (drop old-message-patterns n)))
+                                  (cons (append (car ps) '(psk))
+                                        (cdr ps)))))])])]
+    [_ #f]))
 
-(define/provide NNpsk0 (handshake-pattern "NNpsk0" '() '() '((psk e) (e ee))))
+;; (define/provide NNpsk0 (handshake-pattern "NNpsk0" '() '() '((psk e) (e ee))))
+;; (define/provide NKpsk0 (handshake-pattern "NKpsk0" '() '(s) '((psk e es) (e ee))))
+;; (define/provide IKpsk0 (handshake-pattern "IKpsk0" '() '(s) '((psk e es s ss) (e ee se))))
+;; (define/provide XKpsk0 (handshake-pattern "XKpsk0" '() '(s) '((psk e es) (e ee) (s se))))
+;; (define/provide XXpsk0 (handshake-pattern "XXpsk0" '() '() '((psk e) (e ee s es) (s se))))
+;; (define/provide IXpsk0 (handshake-pattern "IXpsk0" '() '() '((psk e s) (e ee se s es))))
 
-(define/provide NKpsk2 (handshake-pattern "NKpsk2" '() '(s) '((e es) (e ee psk))))
-(define/provide IKpsk2 (handshake-pattern "IKpsk2" '() '(s) '((e es s ss) (e ee se psk))))
+;; (define/provide NKpsk2 (handshake-pattern "NKpsk2" '() '(s) '((e es) (e ee psk))))
+;; (define/provide IKpsk2 (handshake-pattern "IKpsk2" '() '(s) '((e es s ss) (e ee se psk))))
--- noise/tests/run.rkt
+++ noise/tests/run.rkt
@@ -11,38 +11,37 @@
   (require rackunit)
   (require libsodium)
 
-  (file-stream-buffer-mode (current-output-port) 'none) ;; TEMPORARY
+  (file-stream-buffer-mode (current-output-port) 'none)
 
   (define-runtime-path noise-c-basic-path "noise-c-basic.txt")
+  (define-runtime-path snow-path "snow.txt")
 
-  (define noise-c-basic
-    (hash-ref (with-input-from-file noise-c-basic-path read-json) 'vectors))
+  (define all-test-cases
+    (append
+     (hash-ref (with-input-from-file noise-c-basic-path read-json) 'vectors)
+     (hash-ref (with-input-from-file snow-path read-json) 'vectors)))
 
-  (for [(test-case noise-c-basic)]
+  (for [(test-case all-test-cases)]
     (define protocol_name (hash-ref test-case 'protocol_name (lambda () (hash-ref test-case 'name))))
-    (define pattern-name (string->symbol (hash-ref test-case 'pattern)))
-    (define pattern (match pattern-name
-                      ['NN NN]
-                      ['NK NK]
-                      ['IK IK]
-                      ['XK XK]
-                      ['XX XX]
-                      ['IX IX]
-                      [_ #f]))
+    (define pattern
+      (match protocol_name
+        [(pregexp #px"^Noise_([^_]+)_25519_ChaChaPoly_BLAKE2s$" (list _ pattern-name-str))
+         ;; In old-style tests, protocol_name is missing, but name is present; and name
+         ;; sometimes starts with NoisePSK_ in which case it is an old-style PSK test. We skip
+         ;; these.
+         ;; (define is-old-style-psk? (string-prefix? protocol_name "NoisePSK_"))
+         (lookup-handshake-pattern pattern-name-str)]
+        [_ #f]))
     (if (or (hash-ref test-case 'fallback #f)
             (hash-has-key? test-case 'hybrid)
-            (not (equal? (hash-ref test-case 'dh) "25519"))
-            (not (equal? (hash-ref test-case 'cipher) "ChaChaPoly"))
-            (not (equal? (hash-ref test-case 'hash) "BLAKE2s"))
-            (not pattern)
-            (string-prefix? protocol_name "NoisePSK_")
-            )
+            (not pattern))
         (let ()
           ;; (printf "Skipping ~a\n" protocol_name)
           (void))
         (let ()
           (define (! f a . args) (and a (apply f a args)))
           (define (get c k) (! hex-string->bytes (! hash-ref c k #f)))
+          (define (many-psks c k) (match (hash-ref c k '()) ['() #f] [hs (map hex-string->bytes hs)]))
           (printf "Running ~a ~v\n" protocol_name pattern)
           (define (run-test)
             (define init-prologue (get test-case 'init_prologue))
@@ -53,20 +52,22 @@
             (define resp-static (! sk->crypto-box-keypair (get test-case 'resp_static)))
             (define init-remote-static-pk (get test-case 'init_remote_static))
             (define resp-remote-static-pk (get test-case 'resp_remote_static))
-            (define init-psk (get test-case 'init_psk))
-            (define resp-psk (get test-case 'resp_psk))
-            (define I (make-initiator pattern
-                                      #:prologue (or init-prologue #"")
-                                      #:static-keypair init-static
-                                      #:remote-static-pk init-remote-static-pk
-                                      #:pregenerated-ephemeral-keypair init-ephemeral
-                                      #:preshared-keys (and init-psk (list init-psk))))
-            (define R (make-responder pattern
-                                      #:prologue (or resp-prologue #"")
-                                      #:static-keypair resp-static
-                                      #:remote-static-pk resp-remote-static-pk
-                                      #:pregenerated-ephemeral-keypair resp-ephemeral
-                                      #:preshared-keys (and resp-psk (list resp-psk))))
+            (define init-psks (or (! list (get test-case 'init_psk)) (many-psks test-case 'init_psks)))
+            (define resp-psks (or (! list (get test-case 'resp_psk)) (many-psks test-case 'resp_psks)))
+            (define I (make-HandshakeState pattern
+                                           #:role 'initiator
+                                           #:prologue (or init-prologue #"")
+                                           #:static-keypair init-static
+                                           #:remote-static-pk init-remote-static-pk
+                                           #:pregenerated-ephemeral-keypair init-ephemeral
+                                           #:preshared-keys init-psks))
+            (define R (make-HandshakeState pattern
+                                           #:role 'responder
+                                           #:prologue (or resp-prologue #"")
+                                           #:static-keypair resp-static
+                                           #:remote-static-pk resp-remote-static-pk
+                                           #:pregenerated-ephemeral-keypair resp-ephemeral
+                                           #:preshared-keys resp-psks))
             (let loop ((messages (hash-ref test-case 'messages))
                        (sender I)
                        (receiver R)
