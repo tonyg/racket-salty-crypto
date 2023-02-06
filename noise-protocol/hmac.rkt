81f9ce23af9ee493cfdf7947ae0bd1bc7deaabe3
Tony Garnock-Jones <tonyg@leastfixedpoint.com>
2023-02-06 17:00:11 +0100

    HMAC-BLAKE2s example/test

--- noise-protocol/hmac.rkt
+++ noise-protocol/hmac.rkt
@@ -15,8 +15,11 @@
                             (HASH (bytes-append (bytes-xor key ipad) data))))))))
 
 (module+ test
+  (require (only-in file/sha1 hex-string->bytes))
   (require rackunit)
-  (require file/sha1)
   (require file/md5)
+  (require libb2)
+  (check-equal? (subbytes ((make-hmac blake2s BLAKE2S_BLOCKLEN) #"" #"\261\tsyndicate") 0 16)
+                (hex-string->bytes "69ca300c1dbfa08fba692102dd82311a"))
   (check-equal? ((make-hmac (lambda (i) (md5 i #f)) 64) (make-bytes 16 11) #"Hi There")
                 (hex-string->bytes "9294727a3638bb1c13f48ef8158bfc9d")))
